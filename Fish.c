1e076425ff2e37dc7a6d85fa82eb5d32db1785b6
Senchy <senchy5605@hotmail.com>
2012-10-13 15:41:41 +0100

    Did some more work on fish its fully structured just needs to be tested, will do monday hopefull finnish by then/sean/james

--- Fish.c
+++ Fish.c
@@ -2,10 +2,18 @@
 #include <time.h>
 #include <stdlib.h>
 typedef struct{
-char Type;
+Shark* shark;
+Fish* fish;
+}Cell;
+
+typedef struct{
+int SpawnTime;
+}Fish;
+
+typedef struct{
 int SpawnTime;
 int StarveTime;
-}Cell;
+}Shark;
 
 typedef struct{
 bool left;
@@ -14,20 +22,98 @@ bool up;
 bool down;
 }Move;
 
-bool CeckCell(Cell M)
+bool CheckIfEmty(Cell *M)
 {
-	if (M.Type == 'e')
+	if(*M.shark == NULL && *M.fish == NULL)
 	{
 		return true;
 	}
 	return false;
+}
+int ChooseCell(Move Available)
+{
+	bool x = false;
+	int Dirrection;
+	if (Available.left == false &&
+		Available.right == false &&
+		Available.up ==false &&
+		Available.down ==false)
+	{
+		x = true
+		Dirrection = -1;
+	}
+	while(x == false)
+	{
+		srand(time(NULL));
+		int r = rand() / 3;
+		Dirrection = r;
+		switch(r){
+			case(0):
+		    if(Available.left == true)
+				x = true;
+			break;
+			case(1):
+			if(Available.right == true)
+				x = true;
+			break;
+			case(2):
+			if(Available.up == true)
+				x = true;
+			break;
+			case(3):
+			if(Available.down == true)
+				x = true;
+			break;
+		}
+	}
+	return Dirrection;
+}
+
+void FishMove(Cell* Moveto, Cell* fish){
+	if(*fish.SpawnTime == 0)
+	{
+		fish.SpawnTime = 3;
+		Moveto.fish = new Fish;
+		*(Moveto.fish).SpawnTime = 3;
+		return;
+	}
+	Moveto.fish = fish.fish;
+	*(Moveto.fish).SpawnTime--;
+	fish.fish = NULL;
+}
+
+void FishUpdate(Cell * fish, cell[][] * tempworld, int x, int y, int range)
+{   
+	Move Available;
+	int top = y - 1;
+	int bot = y + 1;
+	int left = x - 1;
+	int right = x + 1;
+	if (top < 0)
+		top = range;
+	if (left < 0)
+		left = range;
+	if (right > range)
+		right = 0;
+	if (bot > range)
+		bot = 0;
+	Cell * array[4] = {tempworld[x][top],tempworld[x][bot],tempworld[left][y],tempworld[right][y]}
+	Available.up = CheckCell(array[0])
+	Available.down = CheckCell(array[1])
+	Available.left = CheckCell(array[2])
+	Available.right = CheckCell(array[3])
+	int ChoosenCell = ChooseCell(Available)
+	if (ChooseCell != -1)
+	{
+		FishMove(array[ChooseCell], fish);
+	}
 }
 
 int main(void)
 {
 int NumRows = 10;
 int NumCols = 10;
-Cell world[NumRows][NumCols];
+Cell * world[NumRows][NumCols];
 int i;
 int j;
 for(i=0 i < 10;i++)
@@ -35,21 +121,7 @@ for(i=0 i < 10;i++)
 
 	for(j=0 j < 10;j++)
 	{       
-		Move Available;
-		if(i+1 > 10 || )
-                {
-                      Available.left = CheckCell(world[0][j]);
-   		}
-		else
-		{
-                      Available.left = CheckCell(world[i+1][j]);
-		}
-
-		CheckCell(world[i][j]);
-                
-		srand(time(NULL));
-		int r = rand();
-                
+		FishUpdate(world[i][j], world, i, j, 9);   
 	}
 
 }
