4dbef4264990dcada0f0af843d9f3d8ae1796cff
Senchy <senchy5605@hotmail.com>
2012-10-19 10:06:47 +0100

    work me and james worked on

--- Fish.c
+++ Fish.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+//Cell Structure
 typedef struct{
 Shark* shark;
 Fish* fish;
 }Cell;
 
+//Fish Structure
 typedef struct{
 int SpawnTime;
 }Fish;
 
+//Shark Structure
 typedef struct{
 int SpawnTime;
 int StarveTime;
 }Shark;
 
+//Move Structure hold the availbe places to move
 typedef struct{
 bool left;
 bool right;
@@ -22,6 +26,8 @@ bool up;
 bool down;
 }Move;
 
+//Checks if the pointers to the shark and fish in a give cell are null and
+//returns true if they are both empty
 bool CheckIfEmty(Cell *M)
 {
 	if(*M.shark == NULL && *M.fish == NULL)
@@ -30,6 +36,12 @@ bool CheckIfEmty(Cell *M)
 	}
 	return false;
 }
+
+//checks if there is anywere you can move too, if not then you return -1 to so the fish wont move
+//if there are places that you can move then a random number is generated and saved to a dirrection variable 
+//then checks if that dirrection that you wish to move in is availble
+//if its not then repeats last step till one is found
+//if it is then breaks loop and returns dirrection that we want to move in
 int ChooseCell(Move Available)
 {
 	bool x = false;
@@ -49,7 +61,7 @@ int ChooseCell(Move Available)
 		Dirrection = r;
 		switch(r){
 			case(0):
-		    if(Available.left == true)
+		        if(Available.left == true)
 				x = true;
 			break;
 			case(1):
@@ -98,10 +110,10 @@ void FishUpdate(Cell * fish, cell[][] * tempworld, int x, int y, int range)
 	if (bot > range)
 		bot = 0;
 	Cell * array[4] = {tempworld[x][top],tempworld[x][bot],tempworld[left][y],tempworld[right][y]}
-	Available.up = CheckCell(array[0])
-	Available.down = CheckCell(array[1])
-	Available.left = CheckCell(array[2])
-	Available.right = CheckCell(array[3])
+	Available.up = CheckIfEmty(array[0])
+	Available.down = CheckIfEmty(array[1])
+	Available.left = CheckIfEmty(array[2])
+	Available.right = CheckIfEmty(array[3])
 	int ChoosenCell = ChooseCell(Available)
 	if (ChooseCell != -1)
 	{
